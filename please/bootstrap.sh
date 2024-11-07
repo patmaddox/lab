2619e605f3a85d3fdca4fed8e7d03b2dd7eb2ebb
Pat Maddox <pat@patmaddox.com>
2024-11-07 15:26:58 -0800

    please: build the bootstrapped version

--- please/bootstrap.sh
+++ please/bootstrap.sh
@@ -3,26 +3,54 @@ set -eu
 
 repo="https://github.com/thought-machine/please.git"
 tag="v17.12.4"
-main="master"
-maindir="${main}.jj"
+tagdir="${tag}.jj"
+trunk="master"
+trunkdir="${trunk}.jj"
 
 main() {
     jj::clone
     jj::workspace
+    go::path
+    plz::bootstrap
 }
 
 jj::clone() {
-    if [ ! -d ${maindir} ]; then
-	jj --ignore-immutable git clone ${repo} ${maindir}
+    if [ ! -d ${trunkdir} ]; then
+	jj --ignore-immutable git clone ${repo} ${trunkdir}
     fi
 }
 
 jj::workspace() {
-    local jjdir="${tag}.jj"
-
-    if [ ! -d ${jjdir} ]; then
-	jj -R ${maindir} workspace add --name ${tag} -r ${tag} ${jjdir}
+    if [ ! -d ${tagdir} ]; then
+	jj -R ${trunkdir} workspace add --name ${tag} -r ${tag} ${tagdir}
     fi
+}
+
+go::path() {
+    local gobin
+    local goversion=$(awk '$1 == "go" { sub("\\.", "", $2); print($2) }' ${tagdir}/go.mod)
+
+    if [ -z "${goversion}" ]; then
+	error "no go version found in ${tagdir}/go.mod"
+    fi
+
+    gobin="/usr/local/go${goversion}/bin/go"
+    if [ ! -x "${gobin}" ]; then
+	error "no file ${gobin}"
+    fi
+
+    export PATH=$(dirname ${gobin}):${PATH}
+}
+
+plz::bootstrap() {
+    cd ${tag}.jj
+    ./bootstrap.sh --skip_tests
+}
+
+error() {
+    local message="${1}"
+    echo "E: ${message}" 1>&2
+    exit 1
 }
 
 main "${@}"
