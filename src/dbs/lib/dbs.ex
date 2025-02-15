228b7e3f34cd032df25e69030c21aaa453adc7b9
Pat Maddox <pat@patmaddox.com>
2025-02-15 13:08:00 -0800

    dbs: foundation

--- src/dbs/lib/dbs.ex
+++ src/dbs/lib/dbs.ex
@@ -1,18 +1,9 @@
 defmodule DBS do
-  @moduledoc """
-  Documentation for `DBS`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DBS.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def get(build, store, inputs) do
+    build
+    |> Enum.reduce(Map.merge(store, inputs), fn {k, task}, s ->
+      Map.put_new_lazy(s, k, fn -> task.(s) end)
+    end)
+    |> Map.take(Map.keys(build))
   end
 end
--- src/dbs/test/dbs_test.exs
+++ src/dbs/test/dbs_test.exs
@@ -2,7 +2,21 @@ defmodule DBSTest do
   use ExUnit.Case
   doctest DBS
 
-  test "greets the world" do
-    assert DBS.hello() == :world
+  test "calculate value from input values" do
+    build = %{
+      bar: fn store -> store[:foo] * 2 end,
+      baz: fn _store -> "I am baz" end
+    }
+
+    assert DBS.get(build, %{}, %{foo: 1}) == %{bar: 2, baz: "I am baz"}
+  end
+
+  test "do not re-calculate values" do
+    build = %{
+      bar: fn store -> store[:foo] * 2 end,
+      baz: fn _store -> "I am baz" end
+    }
+
+    assert DBS.get(build, %{bar: 123}, %{foo: 1}) == %{bar: 123, baz: "I am baz"}
   end
 end
