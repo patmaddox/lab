fb928ba396308c08ac6b4003bf5de0035e7923c1
Pat Maddox <pat@patmaddox.com>
2025-02-15 14:31:41 -0800

    dbs: organize tests

--- src/dbs/test/dbs_test.exs
+++ src/dbs/test/dbs_test.exs
@@ -57,35 +57,39 @@ defmodule DBSTest do
     end
   end
 
-  test "calculate value from input values" do
-    store = DBS.new_store()
+  describe "build/3" do
+    test "calculate value from input values" do
+      store = DBS.new_store()
 
-    build =
-      DBS.new_build(%{
-        bar: fn store -> DBS.get(store, :foo) * 2 end,
-        baz: fn _store -> "I am baz" end
-      })
+      build =
+        DBS.new_build(%{
+          bar: fn store -> DBS.get(store, :foo) * 2 end,
+          baz: fn _store -> "I am baz" end
+        })
 
-    assert DBS.build(build, store, %{foo: 1}) == %{bar: 2, baz: "I am baz"}
+      assert DBS.build(build, store, %{foo: 1}) == %{bar: 2, baz: "I am baz"}
+    end
+
+    test "do not re-calculate values" do
+      store = DBS.new_store(%{bar: 123})
+
+      build =
+        DBS.new_build(%{
+          bar: fn store -> DBS.get(store, :foo) * 2 end,
+          baz: fn _store -> "I am baz" end
+        })
+
+      assert DBS.build(build, store, %{foo: 1}) == %{bar: 123, baz: "I am baz"}
+    end
   end
 
-  test "do not re-calculate values" do
-    store = DBS.new_store(%{bar: 123})
+  describe "get/2" do
+    test "raise error on missing value" do
+      store = DBS.new_store()
 
-    build =
-      DBS.new_build(%{
-        bar: fn store -> DBS.get(store, :foo) * 2 end,
-        baz: fn _store -> "I am baz" end
-      })
-
-    assert DBS.build(build, store, %{foo: 1}) == %{bar: 123, baz: "I am baz"}
-  end
-
-  test "raise error on missing value" do
-    store = DBS.new_store()
-
-    assert_raise RuntimeError, ~r/unable to resolve :foo/, fn ->
-      DBS.get(store, :foo)
+      assert_raise RuntimeError, ~r/unable to resolve :foo/, fn ->
+        DBS.get(store, :foo)
+      end
     end
   end
 end
