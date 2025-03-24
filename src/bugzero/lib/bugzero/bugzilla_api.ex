07e3bf1be208b0abdac37a925a083321c8b55ce2
Pat Maddox <pat@patmaddox.com>
2025-03-23 22:24:14 -0700

    bugzero: BugzillaApi.bug/2

--- src/bugzero/lib/bugzero/bugzilla_api.ex
+++ src/bugzero/lib/bugzero/bugzilla_api.ex
@@ -8,17 +8,39 @@ defmodule Bugzero.BugzillaApi do
   end
 
   def version(api) do
+    %Response{status: 200, body: %{"version" => version}} =
+      api
+      |> json_req()
+      |> Req.get!(url: "/version")
+
+    {:ok, version}
+  end
+
+  def bug(api, id) do
+    %Response{status: 200, body: %{"bugs" => [bug]}} =
+      api
+      |> json_req()
+      |> Req.get!(url: "/bug/#{id}?include_fields=_default,tags")
+
+    {:ok, parse_bug(bug)}
+  end
+
+  defp parse_bug(bug) do
+    %{
+      id: Map.fetch!(bug, "id"),
+      tags: Map.fetch!(bug, "tags"),
+      summary: Map.fetch!(bug, "summary")
+    }
+  end
+
+  defp json_req(%__MODULE__{api_key: api_key}) do
     [
       base_url: "https://bugs.freebsd.org/bugzilla/rest",
-      params: [api_key: api.api_key]
+      params: [api_key: api_key]
     ]
     |> Keyword.merge(Application.fetch_env!(:bugzero, :bugzilla_api_options))
     |> Req.new()
     |> Req.Request.put_header("accept", "application/json")
     |> Req.Request.put_header("content-type", "application/json")
-    |> Req.get!(url: "/version")
-    |> case do
-      %Response{status: 200, body: %{"version" => version}} -> {:ok, version}
-    end
   end
 end
--- src/bugzero/test/bugzero/bugzilla_api_test.exs
+++ src/bugzero/test/bugzero/bugzilla_api_test.exs
@@ -7,7 +7,7 @@ defmodule Bugzero.BugzillaApiTest do
     %{api: BugzillaApi.new("api_token")}
   end
 
-  test "version/0", %{api: api} do
+  test "version/1", %{api: api} do
     Req.Test.stub(BugzillaApi, fn conn ->
       assert conn.method == "GET"
       assert conn.request_path == "/bugzilla/rest/version"
@@ -17,5 +17,26 @@ defmodule Bugzero.BugzillaApiTest do
     end)
 
     assert {:ok, "5.0.4.1"} = BugzillaApi.version(api)
+  end
+
+  test "bug/2", %{api: api} do
+    Req.Test.stub(BugzillaApi, fn conn ->
+      assert conn.method == "GET"
+      assert conn.request_path == "/bugzilla/rest/bug/123"
+      assert conn.params == %{"api_key" => "api_token", "include_fields" => "_default,tags"}
+
+      bug_resp = %{
+        "id" => 123,
+        "tags" => ["tag1", "tag2"],
+        "summary" => "bug summary"
+      }
+
+      Req.Test.json(conn, %{"bugs" => [bug_resp]})
+    end)
+
+    assert {:ok, bug} = BugzillaApi.bug(api, 123)
+    assert bug.id == 123
+    assert bug.tags == ["tag1", "tag2"]
+    assert bug.summary == "bug summary"
   end
 end
