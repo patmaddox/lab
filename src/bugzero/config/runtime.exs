42836b51d0eb1a054f6e0f74fbd53a7116bc652b
Pat Maddox <pat@patmaddox.com>
2025-03-25 11:11:45 -0700

    bugzero: run production on localhost

--- src/bugzero/config/runtime.exs
+++ src/bugzero/config/runtime.exs
@@ -52,13 +52,15 @@ if config_env() == :prod do
   config :bugzero, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :bugzero, BugzeroWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: port, scheme: "http"],
+    #    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      #      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: {127, 0, 0, 1},
       port: port
     ],
     secret_key_base: secret_key_base
