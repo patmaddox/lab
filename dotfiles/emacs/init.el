700cb498a200a1b66a24dd532b4c4f2e467f1062
Pat Maddox <pat@patmaddox.com>
2025-04-02 12:24:45 -0700

    emacs: some of my base-level emacs config

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -4,9 +4,45 @@
   (add-to-list 'load-path (expand-file-name "packages/consult-2.2" user-emacs-directory))
   (add-to-list 'load-path (expand-file-name "packages/consult-project-extra-982e800" user-emacs-directory))
 
+  :config
+  (add-hook 'emacs-lisp-mode-hook
+	    (lambda ()
+	      (setq indent-tabs-mode nil)
+	      (define-key emacs-lisp-mode-map
+                          "\C-x\C-e" 'pp-eval-last-sexp)
+	      (add-hook 'before-save-hook 'delete-trailing-whitespace nil t)
+	      (add-hook 'after-save-hook (lambda () (byte-compile-file buffer-file-name)) nil t)))
+
+  (add-to-list 'completion-styles 'flex)
+  (add-to-list 'auto-mode-alist '("\\.jjdescription\\'" . org-mode))
+  (delete ".git/" completion-ignored-extensions)
+  (dolist (i '(".core"
+               ".fslckout"
+               ".so"
+               "auto-save-list/"
+               "history"
+               "ido.last"
+               "transient/"))
+    (add-to-list 'completion-ignored-extensions i))
+  (put 'downcase-region 'disabled nil)
+  (put 'upcase-region 'disabled nil)
+
+  (electric-pair-mode 1)
+  (global-display-line-numbers-mode 1)
+  (global-hl-line-mode 1)
+  (menu-bar-mode -1)
+  (require 'eglot)
+  (show-paren-mode 1)
+
   :bind
   ("M-RET" . project-find-file)
-  ("M-i" . consult-imenu))
+  ("M-i" . consult-imenu)
+  ("M-I" . consult-imenu-multi)
+
+  :custom
+  (load-prefer-newer t)
+  (warning-minimum-level :error)
+  (read-file-name-completion-ignore-case t))
 
 ;; consult - better search and navigation (consulting completing-read)
 ;; https://github.com/minad/consult
