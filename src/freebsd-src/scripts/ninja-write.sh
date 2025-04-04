1b4a132f762be6db5fee8b6c97ba818d5dcdbf8e
Pat Maddox <pat@patmaddox.com>
2025-04-04 13:10:11 -0700

    freebsd-src: write release files to ninja-out

--- src/freebsd-src/scripts/ninja-write.sh
+++ src/freebsd-src/scripts/ninja-write.sh
@@ -2,6 +2,7 @@
 
 bindir=$(dirname $(realpath $0))
 configs=$(ls config/*.conf)
+release_files="MANIFEST base.txz kernel.txz src.txz tests.txz"
 
 cat ${bindir}/ninja.rules
 
@@ -12,6 +13,10 @@ EOF
 
 for c in ${configs}; do
     c=$(basename ${c} .conf)
+    c_release_dir=_build/${c}/amd64.amd64/release
+    c_release_files=$(echo ${release_files} | sed -e "s|^|${c_release_dir}/|" -e "s|[[:space:]+]| ${c_release_dir}/|g")
+    c_release_ninja_dir=ninja-out/${c}
+    c_release_ninja_files=$(echo ${release_files} | sed -e "s|^|${c_release_ninja_dir}/|" -e "s|[[:space:]+]| ${c_release_ninja_dir}/|g")
 
     cat <<EOF
 
@@ -21,8 +26,8 @@ build _build/${c}.build: build-freebsd | config/${c}.conf scripts/build.sh
   config = ${c}
 build ${c}: phony _build/${c}.build
 
-build _build/${c}.release: release-freebsd | _build/${c}.build
+build ${c_release_ninja_files}: release-freebsd | _build/${c}.build
   config = ${c}
-build ${c}/txz: phony _build/${c}.release
+build ${c}/txz: phony ${c_release_ninja_files}
 EOF
 done
