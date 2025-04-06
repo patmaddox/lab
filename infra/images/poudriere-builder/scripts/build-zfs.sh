724b036b5ff31584fa404a1d2b406e9cbb014aa4
Pat Maddox <pat@patmaddox.com>
2025-04-06 14:47:46 -0700

    poudriere-builder: ninja build the image

---
+++ infra/images/poudriere-builder/scripts/build-zfs.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+set -eu
+set -o pipefail
+
+main() {
+    local outfile mdid
+
+    freebsd_txz=${1}; shift
+    outfile=${1}; shift
+    tmpfile=${outfile}.tmp
+    rm -f ${tmpfile}
+    BUILDDIR=$(dirname ${outfile})
+    rootdir=$(realpath ${BUILDDIR}/imgroot)
+    distdir=$(realpath ${BUILDDIR}/../dist/zroot)
+
+    build::validate-txz
+    build::create-zpool
+    build::extract
+    build::config
+    build::export-zpool
+}
+
+build::create-zpool() {
+    truncate -s 20g ${tmpfile}
+    mdid=$(doas mdconfig -a -f ${tmpfile} | grep -o '[[:digit:]]*')
+
+    doas zpool create -m none -o autoexpand=on -t ninja-pb--zroot -R ${rootdir} zroot /dev/md${mdid}
+    doas zfs create -o mountpoint=none ninja-pb--zroot/ROOT
+    doas zfs create -o mountpoint=/ ninja-pb--zroot/ROOT/default
+    doas zfs create -o mountpoint=/home ninja-pb--zroot/home
+    doas zfs create -o mountpoint=/tmp -o exec=on -o setuid=off ninja-pb--zroot/tmp
+    doas zfs create -o mountpoint=/usr -o canmount=off ninja-pb--zroot/usr
+    doas zfs create -o setuid=off ninja-pb--zroot/usr/ports
+    doas zfs create ninja-pb--zroot/usr/src
+    doas zfs create ninja-pb--zroot/usr/obj
+    doas zfs create -o mountpoint=/var -o canmount=off ninja-pb--zroot/var
+    doas zfs create -o setuid=off -o exec=off ninja-pb--zroot/var/audit
+    doas zfs create -o setuid=off -o exec=off ninja-pb--zroot/var/crash
+    doas zfs create -o setuid=off -o exec=off ninja-pb--zroot/var/log
+    doas zfs create -o atime=on ninja-pb--zroot/var/mail
+    doas zfs create -o setuid=off ninja-pb--zroot/var/tmp
+    doas zpool set bootfs=ninja-pb--zroot/ROOT/default ninja-pb--zroot
+
+    doas zfs snapshot -r ninja-pb--zroot@init
+}
+
+build::validate-txz() {
+    local p
+    for p in base kernel; do
+	test -f ${freebsd_txz}/${p}.txz
+    done
+}
+
+build::extract() {
+    local p
+    for p in base kernel; do
+	doas tar -C ${rootdir} -xf ${freebsd_txz}/${p}.txz
+    done
+
+    # I don't want to use the standard build directories.
+    # Make them readonly so I don't inadvertently use them.
+    doas zfs set readonly=on ninja-pb--zroot/usr/src
+    doas zfs set readonly=on ninja-pb--zroot/usr/obj
+
+    doas zfs snapshot -r ninja-pb--zroot@base
+}
+
+build::config() {
+    tar -c -C ${distdir} . | doas tar -x -C ${rootdir} --gid 0 --uid 0
+    doas zfs snapshot -r ninja-pb--zroot@config
+}
+
+build::export-zpool() {
+    doas zpool export ninja-pb--zroot
+    doas mdconfig -d -u ${mdid}
+    mv ${tmpfile} ${outfile}
+}
+
+main "${@}"
