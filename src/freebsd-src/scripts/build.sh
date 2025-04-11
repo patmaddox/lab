272252d21439f588ec6196af51f33e52a428c716
Pat Maddox <pat@patmaddox.com>
2025-04-11 14:18:56 -0700

    freebsd-src: Build src in a temp dir; consolidate build+release
    The FreeBSD build scripts relies on being built in a git repo. This
    works okay with jj when building from main - but when it's a
    workspace, there is no git dir. The git tools are unable to locate git
    information, and read info from the top-level lab repo instead.
    
    This does some gymnastics to create a tag based on the jj head, then
    hard reset to that tag in a temp dir. I combined build+release because
    the jj HEAD tag may not be consistent between runs.
    
    It's a definite workaround the fact that jj doesn't provide a git dir
    to workspaces... yet.
    
    Also: Rename configurations afer their major versions

--- src/freebsd-src/scripts/build.sh
+++ src/freebsd-src/scripts/build.sh
@@ -2,16 +2,74 @@
 set -eu
 set -o pipefail
 
+: ${GIT_MAIN}
+: ${GIT_TMP}
 KERNCONF=${KERNCONF:-GENERIC}
 export KERNCONF
 
 main() {
     parse_args "${@}"
     ensure_not_dirty
+    reset_worktree
 
     _make obj
     OBJDIR=$(cmd::objdir)
     cmd::${CMD}
+}
+
+# FreeBSD build scripts rely on .git being present, but jj worktrees
+# do not have it. Check out the jj workspace to a temporary repo so
+# build scripts correctly identify the git revision.
+#
+# I am not proud of these gymastics.
+#
+# Create a temp tag point to jj @-, which is the actual latest commit,
+# because of how jj uses detached head. Fetch and reset the working
+# tree to @-. Delete the tag so that it doesn't mess with jj.
+reset_worktree() {
+    # ls-files looks in the jj repo. Everything else should be run in
+    # a git repo for the worktree.
+    if [ "${CMD}" = "ls-files" ]; then
+	return 0
+    fi
+
+    local branchname githead
+
+    branchname=$(jj -R ${SRC_ROOT} log -r '::@ & bookmarks()' -n 1 -T 'self.local_bookmarks()' --no-graph)
+    githead=$(jj -R ${SRC_ROOT} log -r "@-" --no-graph -T 'commit_id')
+
+    if [ ! -d ${GIT_TMP} ]; then
+	git clone -q -n ${GIT_MAIN} ${GIT_TMP}
+    fi
+
+    git -C ${GIT_MAIN} tag -f ninja-tmp ${githead}
+
+    git -C ${GIT_TMP} remote update
+    git -C ${GIT_TMP} fetch origin +refs/tags/ninja-tmp:refs/tags/ninja-tmp
+
+    # This builds the kernel identifier with the correct branch name,
+    # even if there are later commits.
+    git -C ${GIT_TMP} checkout ${branchname}
+    git -C ${GIT_TMP} reset --hard ninja-tmp
+
+    git -C ${GIT_MAIN} tag -d ninja-tmp > /dev/null
+
+    # double check that the git repo isn't dirty somehow
+    if [ -n "$(git -C ${GIT_TMP} status --porcelain)" ]; then
+	echo "E: refusing to build a dirty tree"
+	exit 1
+    fi
+
+    SRC_ROOT=$(realpath ${GIT_TMP})
+}
+
+ensure_not_dirty() {
+    set +o pipefail
+    if ! jj -R ${REPO_ROOT} status --quiet | grep -q '^Working copy .* (empty) (no description set)'; then
+	echo "E: refusing to build in a dirty tree"
+	exit 1
+    fi
+    set -o pipefail
 }
 
 parse_args() {
@@ -26,9 +84,6 @@ parse_args() {
     REPO_ROOT=${SRC_ROOT}
 
     case ${CMD} in
-	release|clean-release|objdir-release)
-	    SRC_ROOT=$(realpath ${SRC_ROOT}/release)
-	    ;;
 	build|clean|objdir|ls-files)
 	    ;;
 	*)
@@ -43,23 +98,14 @@ parse_args() {
     CCACHE_CONFIGPATH=$(realpath ccache.conf)
 }
 
-ensure_not_dirty() {
-    set +o pipefail
-    if ! jj -R ${REPO_ROOT} status --quiet | grep -q '^Working copy .* (empty) (no description set)'; then
-	echo "E: refusing to build in a dirty tree"
-	exit 1
-    fi
-    set -o pipefail
-}
-
+# Build and release all in one go so the git ref is consistent.  Clean
+# because we're reusing a temp git dir, which can cause issues as we
+# juggle versions.
 cmd::build() {
+    _make cleanworld
     _make buildworld buildkernel
-}
 
-cmd::release() {
-    # packagesystem does not pick up changes after buildworld, so need
-    # to clean it first
-    _make clean
+    SRC_ROOT=$(realpath ${SRC_ROOT}/release)
     _make -DNOPORTS packagesystem
 }
 
@@ -67,17 +113,8 @@ cmd::clean() {
     _make cleanworld
 }
 
-cmd::clean-release() {
-    _make clean
-}
-
 cmd::objdir() {
     _make -V .OBJDIR
-}
-
-cmd::objdir-release() {
-    # OBJDIR is already set via cmd::objdir in main
-    echo ${OBJDIR}
 }
 
 cmd::ls-files() {
--- src/freebsd-src/scripts/ninja-write.sh
+++ src/freebsd-src/scripts/ninja-write.sh
@@ -7,13 +7,11 @@ configs=$(ls config/*.conf)
 release_files="MANIFEST base.txz kernel.txz src.txz tests.txz"
 src_root="src/freebsd-src"
 
-config_builds=""
 config_releases=""
 
 for c in ${configs}; do
     c=$(basename ${c} .conf)
-    config_builds="mod.freebsd.build.${c} ${config_builds}"
-    config_releases="mod.freebsd.release.${c} ${config_releases}"
+    config_releases="mod.freebsd.${c} ${config_releases}"
 done
 
 cat ${bindir}/ninja.rules
@@ -22,12 +20,7 @@ cat <<EOF
 
 # auto-generated from config/*.conf
 
-#build mod.freebsd.build: phony ${config_builds}
-#build freebsd.build: phony mod.freebsd.build
-
-build mod.freebsd.release: phony ${config_releases}
-#build freebsd.release: phony mod.freebsd.release
-build freebsd.all: phony mod.freebsd.release
+build mod.pkg.freebsd: phony ${config_releases}
 
 EOF
 
@@ -42,15 +35,10 @@ for c in ${configs}; do
 
 ## ${c}
 
-build ${src_root}/_build/${c}.build: build-freebsd | ${src_root}/config/${c}.conf ${src_root}/scripts/build.sh
+build ${c_release_ninja_files}: build-freebsd | ${src_root}/config/${c}.conf ${src_root}/scripts/build.sh ${src_root}/src.conf
   config = ${c}
-build mod.freebsd.build.${c}: phony ${src_root}/_build/${c}.build
-#build freebsd.build.${c}: phony mod.freebsd.build.${c}
-
-build ${c_release_ninja_files}: release-freebsd | mod.freebsd.build.${c}
-  config = ${c}
-build mod.freebsd.release.${c}: phony ${c_release_ninja_files}
-#build freebsd.release.${c}: phony mod.freebsd.release.${c}
-build freebsd.${c}: phony mod.freebsd.release.${c}
+  stampfile = ${src_root}/_build/${c}.build
+build mod.freebsd.${c}: phony ${c_release_ninja_files}
+build freebsd.${c}: phony mod.freebsd.${c}
 EOF
 done
