4887dd529a0d91d1fed19864043a150f1b343110
Pat Maddox <pat@patmaddox.com>
2025-04-15 11:00:31 -0700

    images/gulliver: Build image using ninja

---
+++ infra/images/gulliver/scripts/build-be.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+set -o pipefail
+
+main() {
+    local image_name jail_name portsfile outfile
+
+    image_name=${1:?$(usage)}; shift
+    jail_name=${1:?$(usage)}; shift
+    portsfile=${1:?$(usage)}; shift
+    outfile=${1:?$(usage)}; shift
+
+    scp ${portsfile} root@poudriere:/tmp/${image_name}-${jail_name}.ports
+    ssh -t root@poudriere "poudriere image -j ${jail_name} -t zfs+send+be -f /tmp/${image_name}-${jail_name}.ports -n ${image_name}${jail_name} -s 10G"
+    scp root@poudriere:/usr/local/poudriere/data/images/${image_name}${jail_name}.be.zfs ${outfile}.tmp
+    mv ${outfile}.tmp ${outfile}
+}
+
+usage() {
+    echo "Usage: build-be.sh <image_name> <jail_name> <portsfile> <outfile>"
+}
+
+main "${@}"
