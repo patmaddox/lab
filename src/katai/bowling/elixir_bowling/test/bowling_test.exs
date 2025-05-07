1155fa243d491d7eb166b5b250e5d542fd827bcf
Pat Maddox <pat@patmaddox.com>
2025-05-07 09:48:31 -0700

    katai/bowling: Update tests to check for frames first
    I think frames are simpler and provide faster feedback than score.

--- src/katai/bowling/elixir_bowling/test/bowling_test.exs
+++ src/katai/bowling/elixir_bowling/test/bowling_test.exs
@@ -5,8 +5,8 @@ defmodule BowlingTest do
   test "new game has no score or frames" do
     game = %Bowling{}
 
-    assert is_nil(game.score)
     assert game.frames == []
+    assert is_nil(game.score)
   end
 
   test "one incomplete frame" do
@@ -14,8 +14,8 @@ defmodule BowlingTest do
       %Bowling{}
       |> Bowling.roll(1)
 
-    assert is_nil(game.score)
     assert game.frames == [[1]]
+    assert is_nil(game.score)
   end
 
   test "one complete frame" do
@@ -24,8 +24,8 @@ defmodule BowlingTest do
       |> Bowling.roll(1)
       |> Bowling.roll(2)
 
-    assert game.score == 3
     assert game.frames == [[1, 2]]
+    assert game.score == 3
   end
 
   test "two frames, with one incomplete" do
@@ -35,8 +35,8 @@ defmodule BowlingTest do
       |> Bowling.roll(2)
       |> Bowling.roll(3)
 
-    assert game.score == 3
     assert game.frames == [[1, 2], [3]]
+    assert game.score == 3
   end
 
   test "frame with one miss" do
@@ -45,8 +45,8 @@ defmodule BowlingTest do
       |> Bowling.roll(0)
       |> Bowling.roll(1)
 
-    assert game.score == 1
     assert game.frames == [[0, 1]]
+    assert game.score == 1
   end
 
   test "frame with two misses" do
@@ -55,8 +55,8 @@ defmodule BowlingTest do
       |> Bowling.roll(0)
       |> Bowling.roll(0)
 
-    assert game.score == 0
     assert game.frames == [[0, 0]]
+    assert game.score == 0
   end
 
   test "two complete frames" do
@@ -67,8 +67,8 @@ defmodule BowlingTest do
       |> Bowling.roll(3)
       |> Bowling.roll(4)
 
-    assert game.score == 10
     assert game.frames == [[1, 2], [3, 4]]
+    assert game.score == 10
   end
 
   test "incomplete spare frame" do
@@ -77,8 +77,8 @@ defmodule BowlingTest do
       |> Bowling.roll(9)
       |> Bowling.roll(1)
 
-    assert is_nil(game.score)
     assert game.frames == [[9, 1]]
+    assert is_nil(game.score)
   end
 
   test "complete spare frame" do
@@ -88,8 +88,8 @@ defmodule BowlingTest do
       |> Bowling.roll(1)
       |> Bowling.roll(3)
 
-    assert game.score == 13
     assert game.frames == [[9, 1], [3]]
+    assert game.score == 13
   end
 
   test "complete spare frame followed by complete frame" do
@@ -100,7 +100,7 @@ defmodule BowlingTest do
       |> Bowling.roll(3)
       |> Bowling.roll(4)
 
-    assert game.score == 20
     assert game.frames == [[9, 1], [3, 4]]
+    assert game.score == 20
   end
 end
