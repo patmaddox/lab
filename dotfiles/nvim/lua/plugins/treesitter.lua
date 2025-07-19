1d5439aec02189c4dce3de4b4c285222e3c9c2b4
Pat Maddox <pat@patmaddox.com>
2025-07-19 14:46:38 -0700

    nvim: Add elixir treesitter config

---
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "elixir", "heex", "eex" },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+	end,
+}
