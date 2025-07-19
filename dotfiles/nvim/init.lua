0abe94ad6ccadee0a8b617a989db8797ea4f3af0
Pat Maddox <pat@patmaddox.com>
2025-07-19 15:27:20 -0700

    nvim: neotest with elixir config

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -7,9 +7,16 @@ vim.opt.expandtab = true -- spaces
 
 require('config.lazy')
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>fj', builtin.git_files, { desc = 'Telescope jj files (currently git)' })
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fj', telescope.git_files, { desc = 'Telescope jj files (currently git)' })
+
+local neotest = require('neotest')
+vim.keymap.set('n', '<leader>tt', function() neotest.run.run() end, { desc = "Run nearest test" })
+vim.keymap.set('n', '<leader>tf', function() neotest.run.run(vim.fn.expand("%")) end, { desc = "Run current file" })
+vim.keymap.set('n', '<leader>ta', function() neotest.run.run(vim.fn.getcwd()) end, { desc = "Run all tests" })
+vim.keymap.set('n', '<leader>to', function() neotest.output.open({ enter = true }) end, { desc = "Show test output" })
+vim.keymap.set('n', '<leader>ts', function() neotest.summary.toggle() end, { desc = "Toggle test summary" })
