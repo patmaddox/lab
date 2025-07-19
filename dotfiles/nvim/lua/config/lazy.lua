47b83adb1b2024a3ada1152902a00317c3d1fd05
Pat Maddox <pat@patmaddox.com>
2025-07-19 14:11:37 -0700

    nvim: basic lazy + telescope setup

---
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -0,0 +1,11 @@
+local lazypath = vim.fn.stdpath("config") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
