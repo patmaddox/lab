a27141b7d00c4c4dc12a14bb80fdf6c90cc82956
Pat Maddox <pat@patmaddox.com>
2025-07-19 14:25:13 -0700

    nvim: Some basic keybindings

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,1 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 require('config.lazy')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fj', builtin.git_files, { desc = 'Telescope jj files (currently git)' })
