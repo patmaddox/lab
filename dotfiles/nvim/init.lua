18ab1721b1d1d0ba008512a189b2565a63e18076
Pat Maddox <pat@patmaddox.com>
2025-07-21 11:49:00 -0700

    nvim: Configure shell scripts to match FreeBSD indentation conventions

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,12 +1,22 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.relativenumber = true
+vim.opt.number = true
+
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true -- spaces
 
-vim.opt.relativenumber = true
-vim.opt.number = true
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sh", "bash", "zsh" },
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.softtabstop = 8
+  end,
+})
 
 require('config.lazy')
 
