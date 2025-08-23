922d6819c5cb672b67a8038c5d2d3f9c13891136
Pat Maddox <pat@patmaddox.com>
2025-08-22 23:49:44 -0700

    just/fmt: Strip trailing whitespace

--- libexec/just/fmt.sh
+++ libexec/just/fmt.sh
@@ -6,6 +6,8 @@ jj st | awk '/^[A|M] / {print $2}' | while IFS= read -r file; do
         continue
     fi
 
+    sed -i '' 's/[[:space:]]*$//' "$file"
+
     if [ "$(tail -c 1 "$file" | wc -l)" -eq 0 ]; then
         echo >> "$file"
     fi
