150145fad9f0030d13b2111e924db8214134ce92
Pat Maddox <pat@patmaddox.com>
2025-09-12 22:32:01 -0700

    nvim: Elixir LS format on save

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,17 @@ return {
 
     lspconfig.elixirls.setup({
       cmd = { vim.fn.expand("~/.emacs.dev/dist/elixir-ls-v0.27.2/language_server.sh") },
+      on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("ElixirFormat", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
 
     lspconfig.clangd.setup({
