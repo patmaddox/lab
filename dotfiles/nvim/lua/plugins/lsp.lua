87a3ea2c1f2d5aca3c235e5bde76a022200b9e6a
Pat Maddox <pat@patmaddox.com>
2025-10-14 11:09:04 -0700

    nvim: Bump package versions and update LSP config
    LSP package has breaking changes, update config to work with new API.

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,37 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    local lspconfig = require("lspconfig")
-
-    lspconfig.elixirls.setup({
+    -- Configure elixirls
+    vim.lsp.config.elixirls = {
       cmd = { vim.fn.expand("~/.emacs.dev/dist/elixir-ls-v0.27.2/language_server.sh") },
-      on_attach = function(client, bufnr)
-        if client.server_capabilities.documentFormattingProvider then
+      filetypes = { "elixir", "eelixir", "heex", "surface" },
+      root_dir = vim.fs.root(0, { "mix.exs" }),
+    }
+
+    -- Configure clangd
+    vim.lsp.config.clangd = {
+      cmd = { "/usr/local/llvm19/bin/clangd" },
+      filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+      root_dir = vim.fs.root(0, { ".git", "compile_commands.json", "compile_flags.txt" }),
+    }
+
+    -- Enable LSP servers
+    vim.lsp.enable({ "elixirls", "clangd" })
+
+    -- Set up format on save for Elixir files
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("ElixirFormat", { clear = true }),
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.name == "elixirls" and client.supports_method("textDocument/formatting") then
           vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("ElixirFormat", { clear = true }),
-            buffer = bufnr,
+            buffer = args.buf,
             callback = function()
               vim.lsp.buf.format({ async = false })
             end,
           })
         end
       end,
-    })
-
-    lspconfig.clangd.setup({
-      cmd = { "/usr/local/llvm19/bin/clangd" }
     })
   end,
 }
