80f521c7c2fb2fa5f437c47f950133832f2a79c2
Pat Maddox <pat@patmaddox.com>
2025-10-24 18:21:47 -0700

    emacs: I no longer build all the packages here
    I've ported the packages I care about to freebsd-ports.
    
    Change-Id: I027ac48afb91fe2a403b032503b33ab56a6a6964

--- dotfiles/emacs/tests.sh
+++ dotfiles/emacs/tests.sh
@@ -6,8 +6,7 @@ atf_init_test_cases() {
 }
 
 helper::install() {
-    atf_check -o ignore -e ignore make -C ${src_dir} -s
-    atf_check make -C ${src_dir} -s install
+    atf_check make -C ${src_dir} -s
 }
 
 ## install_and_load
@@ -24,7 +23,6 @@ install_and_load_body() {
     # compile everything except for init.el
     atf_check test -f ${HOME}/.emacs.d/init.el
     atf_check test ! -f ${HOME}/.emacs.d/init.elc
-    atf_check -o not-empty find ${HOME}/.emacs.d/packages -name '*.elc'
 
     # run the elisp check that files load with correct modes etc
     # emacs can be noisy, so just focus on error code
@@ -36,6 +34,6 @@ install_and_load_body() {
     # make sure install doesn't overwrite an existing dir
     rm -rf ${HOME}/.emacs.d
     mkdir ${HOME}/.emacs.d
-    atf_check -s exit:1 -o ignore -e match:"E: DESTDIR already exists" make -C ${src_dir} -s install
+    atf_check -s exit:1 -o ignore -e match:"E: DESTDIR already exists" make -C ${src_dir} -s
     atf_check -o empty find ${HOME}/.emacs.d -type f
 }
