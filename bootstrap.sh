85771000432634879efad6e101e88b9a4a7170ea
Pat Maddox <pat@patmaddox.com>
2026-01-30 18:47:36 -0800

    bootstrap: Unconditionally build and install redo-c
    If I've made any changes, I want to pick them up.
    
    Change-Id: I50ee33caa0d4e3e3db949eaf049388f46a6a6964

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,7 @@ set -o pipefail
 
 jjroot=$(jj root)
 
-if [ ! -f bin/redo-c/redo ]; then
-    mkdir -p bin/redo-c
-    cd oss/redo-c
-    ./bootstrap.sh
-    cp redo-always redo-ifchange redo-ifcreate redo ${jjroot}/bin/redo-c/
-fi
+mkdir -p bin/redo-c
+cd oss/redo-c
+./bootstrap.sh
+cp redo-always redo-ifchange redo-ifcreate redo ${jjroot}/bin/redo-c/
