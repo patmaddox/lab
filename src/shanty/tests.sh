6fa8d95188ee4c8217882097c1dd6bf4c33a8d8e
Pat Maddox <pat@patmaddox.com>
2026-02-23 18:48:21 -0800

    shanty: Add a test for stdio stream
    Change-Id: If4f1dde2c58db2028829b6897cbe3c626a6a6964

--- src/shanty/tests.sh
+++ src/shanty/tests.sh
@@ -3,14 +3,23 @@ src_dir=$(atf_get_srcdir)
 
 atf_init_test_cases() {
 	atf_add_test_case basic
+	atf_add_test_case stream
 }
 
 atf_test_case basic
 basic_head() {
 	atf_set "descr" "Basic shanty"
 }
-
 basic_body() {
 	atf_check ${src_dir}/shanty ${src_dir}/fixtures/basic.shanty
+	atf_check -o inline:"foo local\nfoo remote\nbar remote\n" cat foo.remote
+}
+
+atf_test_case stream
+stream_head() {
+	atf_set "descr" "Stream shanty"
+}
+stream_body() {
+	atf_check ${src_dir}/shanty ${src_dir}/fixtures/stream.shanty
 	atf_check -o inline:"foo local\nfoo remote\nbar remote\n" cat foo.remote
 }
