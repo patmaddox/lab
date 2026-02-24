147032eaa67129286bcfecd26ceebd8360a2bf46
Pat Maddox <pat@patmaddox.com>
2026-02-23 18:58:57 -0800

    shanty: Add pull mode
    Default mode is to push from here to there. It may be preferable in
    some cases to pull from there to here. An example is for importing
    config from a remote server to local version control:
    
    copy_config__stream() {
        tar -C / -c /tmp/config.mtree
    }
    copy_config__here() {
        tar -C dist -x
    }
    
    Change-Id: Ic009c9209c813c7a2d83b5fec585624e6a6a6964

--- src/shanty/tests.sh
+++ src/shanty/tests.sh
@@ -4,6 +4,7 @@ src_dir=$(atf_get_srcdir)
 atf_init_test_cases() {
 	atf_add_test_case basic
 	atf_add_test_case stream
+	atf_add_test_case pull
 }
 
 atf_test_case basic
@@ -22,4 +23,13 @@ stream_head() {
 stream_body() {
 	atf_check ${src_dir}/shanty ${src_dir}/fixtures/stream.shanty
 	atf_check -o inline:"foo local\nfoo remote\nbar remote\n" cat foo.remote
+}
+
+atf_test_case pull
+pull_head() {
+	atf_set "descr" "Pull shanty"
+}
+pull_body() {
+	atf_check ${src_dir}/shanty ${src_dir}/fixtures/pull.shanty
+	atf_check -o inline:"foo remote\nfoo local\nbar remote\nbar local\n" cat foo.local
 }
