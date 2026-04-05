6b46ccb64d27a4104c5900b14475e6a297186cf8
Pat Maddox <pat@patmaddox.com>
2026-04-05 03:16:56 -0700

    emacs/notmuch: Visually highlight new and flagged emails

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -567,6 +567,11 @@ and prefixes every line with \"> \"."
 (setq notmuch-search-oldest-first nil)
 (setq notmuch-show-only-matching-messages t)
 (setq notmuch-show-mark-read-tags nil)
+(setq notmuch-search-line-faces '(("new" :weight bold)))
+(setq notmuch-tag-formats
+      '(("new" (propertize tag 'face 'notmuch-tag-unread))
+        ("flagged" (propertize tag 'face 'notmuch-tag-flagged)
+         (notmuch-tag-format-image-data tag (notmuch-tag-star-icon)))))
 (setq notmuch-saved-searches
       '((:name "my inbox" :query "tag:new not tag:patch not tag:flagged not to:claude" :key "i")
         (:name "claude inbox" :query "tag:new and to:claude not tag:draft" :key "c")
