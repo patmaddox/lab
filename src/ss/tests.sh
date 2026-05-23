c597428608ff80703b03275f9fc4ec58b59cdfb9
Pat Maddox <pat@patmaddox.com>
2026-05-23 01:08:05 -0700

    ss: rebuild clean

--- src/ss/tests.sh
+++ src/ss/tests.sh
@@ -4,6 +4,7 @@ export PATH=$(atf_get_srcdir):$PATH
 
 atf_init_test_cases() {
 	atf_add_test_case hello_world
+	atf_add_test_case hello_world_rebuild_clean
 	atf_add_test_case hello_world_vars
 	atf_add_test_case named_build_function
 }
@@ -17,6 +18,20 @@ hello_world_body() {
 	cd work
 	atf_check -s eq:0 -e inline:"hello\n" ss
 	atf_check -s eq:0 -o inline:"hello world\n" ./hello
+}
+
+atf_test_case hello_world_rebuild_clean
+hello_world_rebuild_clean_head() {
+	atf_set "descr" "Rebuild a target with no changed deps"
+}
+hello_world_rebuild_clean_body() {
+	cp -r "$(atf_get_srcdir)/examples/1_hello_world" work
+	cd work
+	atf_check -s eq:0 -e inline:"hello\n" ss
+	atf_check -s eq:0 -o inline:"hello world\n" ./hello
+
+	echo "rebuilding..."
+	atf_check -s eq:0 ss
 }
 
 atf_test_case hello_world_vars
