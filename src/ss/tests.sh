932a292b3b30e532d5e631c58ec7a993f65eb02e
Pat Maddox <pat@patmaddox.com>
2026-05-23 00:47:55 -0700

    ss: hello world with vars

--- src/ss/tests.sh
+++ src/ss/tests.sh
@@ -4,6 +4,7 @@ export PATH=$(atf_get_srcdir):$PATH
 
 atf_init_test_cases() {
 	atf_add_test_case hello_world
+	atf_add_test_case hello_world_vars
 }
 
 atf_test_case hello_world
@@ -12,6 +13,17 @@ hello_world_head() {
 }
 hello_world_body() {
 	cp -r "$(atf_get_srcdir)/examples/1_hello_world" work
+	cd work
+	atf_check -s eq:0 -e inline:"hello\n" ss
+	atf_check -s eq:0 -o inline:"hello world\n" ./hello
+}
+
+atf_test_case hello_world_vars
+hello_world_vars_head() {
+	atf_set "descr" "Build a target using framework vars"
+}
+hello_world_vars_body() {
+	cp -r "$(atf_get_srcdir)/examples/2_hello_world_vars" work
 	cd work
 	atf_check -s eq:0 -e inline:"hello\n" ss
 	atf_check -s eq:0 -o inline:"hello world\n" ./hello
